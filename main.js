0b60d2745c304ea7528d466f8cea662ed0042144
pblerial <dprial44@gmail.com>
2023-04-07 15:36:01 +0200

    Vue create App

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data() {
+            return {
+                product : 'Stocks',
+                desc: 'This is the most amzing socks for developer'
+            }
+        },
+})
