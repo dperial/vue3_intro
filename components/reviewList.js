ec0699d696ad04e04520af933996ab93a3c5e4e3
pblerial <dprial44@gmail.com>
2023-04-17 11:36:57 +0200

    error solve in form

--- components/reviewList.js
+++ components/reviewList.js
@@ -1,17 +1,24 @@
 app.component('review-list', {
+    props: {
+        reviews: {
+            type: Array,
+            required: true
+        }
+    },
     template:
 
     /*html*/
     `
     <div class="review-container">
-        <h3> Review: </h3>
+        <h3> Reviews: </h3>
         <ul>
-            <li v-for="(review, index) in reviews" :kex='index'> {{ review.name }} gave this {{ review.rating }}
+            <li v-for="(review, index) in reviews" :key='index'> {{ review.name }} gave this {{ review.rating }} stars
             <br/>
             "{{review.review}}"
-            </li>
+            
             <br/>
-            recommend: {{review.recommend}}
+            Recommended: {{review.recommend}}
+            </li>
         </ul>
     </div>
     ` 
